3441b83840f28ec32b165eeb0125cd09ea6c8c19
Serhii Onopriienko <seriyrayne@gmail.com>
2023-08-31 23:49:50 +0200

    donateButton

--- src/components/DonateButton.jsx
+++ src/components/DonateButton.jsx
@@ -1,3 +1,16 @@
-export const DonateButton = () => {
-   return <div>donate button</div>;
+import '../styles/components/_donate-button.scss';
+
+export const DonateButton = ({ buttonClass }) => {
+  return (
+    <button className={buttonClass}>
+      <a href='#' target='_blank'>
+        ЗРОБИТИ ДОНАТ
+      </a>
+    </button>
+  );
 };
+
+// для використання цього компоненту необхідно при його вставці додати props -
+// buttonClass, який відповідає розмірам кнопки. Існує 3 класи, які
+// мають медіаправила: для хедера "headerButton", для основного контенту
+// "mainButton" і для бургерного меню "burger".
