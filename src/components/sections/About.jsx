3fb280575f30178868b3f60dbbbbb580030030ba
Elena_Remennaya <elena.oleksenko12@gmail.com>
2023-09-01 23:24:22 +0300

    fixed styles

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,27 +1,26 @@
-import '../../styles/components/_about.scss';
 import { DonateButton } from '../DonateButton';
 
 export const About = () => {
    return (
-   <section className="about_section">
-      <div className="about_wrapper">
-         <div className="about_title">Про бригаду</div>
-         <div className="about_description_wrapper">
-         <div className="about_description_container">
-            <p className="about_description_text">
+   <section className="about-section">
+      <div className="about-wrapper">
+         <div className="about-title">Про бригаду</div>
+         <div className="about-description-wrapper">
+         <div className="about-description-container">
+            <p className="about-description-text">
             57-ма окрема мотопіхотна бригада імені кошового отамана Костя 
             Гордієнка була сформована вже після початку російської агресії, 
             восени 2014 року. До її складу увійшли три батальйони, які також 
             були сформовані вже після початку війни: 17-й, 34-й та 42-й мотопіхотні. 
             Взимку 2015 року підрозділи бригади воювали під Дебальцевим.
             </p>
-            <div className="about_btn_container">
+            <div className="about-btn-container">
             <DonateButton buttonClass={'mainButton'} />
          </div>
          </div>
-         <div className="about_photo_wrapper">
-       <div className="about_photo_container">
-        <div className="about_photo"></div>
+         <div className="about-photo-wrapper">
+       <div className="about-photo-container">
+        <div className="about-photo"></div>
        </div>
       </div>
 
