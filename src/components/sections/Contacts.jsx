65eb0d1bb307f0f4ebacf202bf6541962576a3df
Oleksandr <admin@admins-MacBook-Pro.local>
2023-09-02 00:25:51 +0300

    Add Contacts

--- src/components/sections/Contacts.jsx
+++ src/components/sections/Contacts.jsx
@@ -1,3 +1,16 @@
 export const Contacts = () => {
-   return <div>this is contacts</div>;
+   return (
+   <div>
+   <div className="container"> 
+      <div className="contact-box">
+          <h2>Звернутись до організаторів</h2>
+          <p>Якщо готові скластись dokoopy і долучитись до хорошої справи – пишіть прямо зараз отсюди: </p>
+          <div className="contact-link">
+            <a href="https://t.me/+CBXkAJlsCy83ZDYy">telegram</a>
+            <a href="mailto:info@baza-trainee.tech">email</a>
+          </div>
+      </div>
+   </div>
+   </div>
+   );
 };
