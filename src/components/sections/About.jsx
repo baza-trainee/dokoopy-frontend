4a7680f2bfa34b29f3a7ac79f2d669201d6371e2
Elena_Remennaya <elena.oleksenko12@gmail.com>
2023-09-01 19:46:49 +0300

    add mobile, tablet version

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,3 +1,35 @@
+import '../../styles/components/_about.scss';
+import { DonateButton } from '../DonateButton';
+
 export const About = () => {
-   return <div>this is about</div>;
+   return (
+   <section className="about_section">
+      <div className="about_wrapper">
+         <div className="about_title">Про бригаду</div>
+         <div className="about_description_wrapper">
+         <div className="about_description_container">
+            <p className="about_description_text">
+            57-ма окрема мотопіхотна бригада імені кошового отамана Костя 
+            Гордієнка була сформована вже після початку російської агресії, 
+            восени 2014 року. До її складу увійшли три батальйони, які також 
+            були сформовані вже після початку війни: 17-й, 34-й та 42-й мотопіхотні. 
+            Взимку 2015 року підрозділи бригади воювали під Дебальцевим.
+            </p>
+            <div className="about_btn_container">
+            <DonateButton buttonClass={'mainButton'} />
+         </div>
+         </div>
+         <div className="about_photo_wrapper">
+       <div className="about_photo_container">
+        <div className="about_photo"></div>
+       </div>
+      </div>
+
+         </div>
+         
+         
+      </div>
+      
+   </section>
+   );
 };
