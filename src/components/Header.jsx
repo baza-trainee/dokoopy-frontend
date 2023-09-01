c79358964ae4cf5e5a6103c246b5af3a1e5ab595
AscariA10 <109697703+AscariA10@users.noreply.github.com>
2023-09-01 14:52:59 +0300

    add some styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,16 @@
 export const Header = () => {
-   return <div className="header">This is header</div>;
+   return (
+      <div className="container header">
+         <p className="logo">dokoopy</p>
+         <nav>
+            <ul>
+               <li></li>
+               <li></li>
+               <li></li>
+            </ul>
+         </nav>
+         <button>donate</button>
+         <p>UA</p>
+      </div>
+   );
 };
