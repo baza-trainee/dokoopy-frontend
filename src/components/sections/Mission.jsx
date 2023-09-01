323939f033def60746ace377a3c6d8b5d1c69ba9
Oleksandr <admin@admins-MacBook-Pro.local>
2023-09-01 14:09:00 +0300

    Add page Mission

--- src/components/sections/Mission.jsx
+++ src/components/sections/Mission.jsx
@@ -1,3 +1,31 @@
 export const Mission = () => {
-   return <div>this is mission</div>;
+   return <div className="Mission">
+      <div className="Mission-title">
+         <h2>
+            Наша мета
+         </h2>
+      </div>
+            <div className="Mission-container">
+               <div className="Mission-container-box">
+                  <div className="Mission-container-box-img">
+
+                  </div>
+                  <div className="Mission-container-box-text">
+                     <p>
+                     Збір на 57 бригаду, котрі беруть участь в самих запеклих боях. Тому потреба в розхідних матеріалах просто вееелеетенська - це і рації, і ремонт машин, гума на колеса тощо. Саме тому у нас немає запиту на конкретну суму - на фронті щось потрібно постійно, фінансове питання відкрите завжди.
+                     </p>
+                  </div>
+               </div>
+               <div className="Mission-container-box">
+                   <div className="Mission-container-box-img">
+
+                  </div>
+                  <div className="Mission-container-box-text">
+                     <p>
+                     Ми віримо: якщо спроможні бізнеси, люди та корпорації візьмуть на себе фінансову підтримку хоча б одного військового - потреби фронту будуть закриватися швидше. На цьому сайті ми – Don’t Panic Group та усі, кому відгукується ідея, збираємось до купи, щоб підтримати 57 бригаду.
+                     </p>
+                  </div>
+               </div>          
+            </div>
+          </div>;
 };
