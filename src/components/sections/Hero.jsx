bb7e920ec1e84def34dbf03bf8121b498e6aadf0
AscariA10 <109697703+AscariA10@users.noreply.github.com>
2023-09-02 00:14:06 +0300

    fixes

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,7 +1,7 @@
 export const Hero = () => {
    return (
       <div className="hero">
-         <div className="container">
+         <div className="container container_hero">
             <h1 className="hero-title">
                Збір на 57 бригаду, котрі беруть участь в найзапекліших боях
             </h1>
