58af5f2272a14ec0f629fcfa85074d467482316e
Oleksandr <admin@admins-MacBook-Pro.local>
2023-09-02 20:44:22 +0300

    Add partner

--- src/components/sections/Partners.jsx
+++ src/components/sections/Partners.jsx
@@ -1,15 +1,26 @@
 export const Partners = () => {
    return (
-      <div>
+      <div className="partners">
             <div className="container">
                <div className="partner">
                   <h2 className="title">Наші партнери</h2>
                <div class="partner-list">
-                  <div class="partner-card"></div>
-                  <div class="partner-card"></div>
-                  <div class="partner-card"></div>
-                  <div class="partner-card"></div>
-                  <div class="partner-card"></div>
+                  <div>
+                     <span>
+                     <a href="#"><img src="../../../public/images/logo-baza.png" alt="logo" /></a>
+                     <a href="#"><img src="../../../public/images/logo-baza.png" alt="logo" /></a>
+                     <a href="#"><img src="../../../public/images/logo-baza.png" alt="logo" /></a>
+                     <a href="#"><img src="../../../public/images/logo-baza.png" alt="logo" /></a>
+                     <a href="#"><img src="../../../public/images/logo-baza.png" alt="logo" /></a>
+                     </span>
+                     <span>
+                     <a href="#"><img src="../../../public/images/logo-baza.png" alt="logo" /></a>
+                     <a href="#"><img src="../../../public/images/logo-baza.png" alt="logo" /></a>
+                     <a href="#"><img src="../../../public/images/logo-baza.png" alt="logo" /></a>
+                     <a href="#"><img src="../../../public/images/logo-baza.png" alt="logo" /></a>
+                     <a href="#"><img src="../../../public/images/logo-baza.png" alt="logo" /></a>
+                     </span>
+                  </div>
                </div>
             </div>
          </div>
