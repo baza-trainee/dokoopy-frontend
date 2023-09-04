58cd01513018883ee5c3c98ffbbf5baeb1d236fc
Andrii <andriy30534>
2023-09-04 18:43:54 +0300

    add react slic

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,11 +1,11 @@
+import MySlider from './Slider'
+
 export const Hero = () => {
-   return (
-      <div className="hero">
-         <div className="container container_hero">
-            <h1 className="hero-title">
-               Збір на 57 бригаду, котрі беруть участь в найзапекліших боях
-            </h1>
-         </div>
-      </div>
-   );
-};
+	return (
+		<div className='hero'>
+			<div className='container container_hero'>
+				<MySlider />
+			</div>
+		</div>
+	)
+}
---
+++ src/components/sections/Slider.jsx
@@ -0,0 +1,37 @@
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick-theme.css'
+import 'slick-carousel/slick/slick.css'
+export default function MySlider() {
+	const settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+	}
+	return (
+		<div>
+			<h2> Single Item</h2>
+			<Slider {...settings}>
+				<div>
+					<h3>1</h3>
+				</div>
+				<div>
+					<h3>2</h3>
+				</div>
+				<div>
+					<h3>3</h3>
+				</div>
+				<div>
+					<h3>4</h3>
+				</div>
+				<div>
+					<h3>5</h3>
+				</div>
+				<div>
+					<h3>6</h3>
+				</div>
+			</Slider>
+		</div>
+	)
+}
