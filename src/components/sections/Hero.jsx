036ab75f71cda4f362cc358b99619644d9c42e85
Andrii <andriy30534>
2023-09-04 20:11:39 +0300

    add arow

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,11 +1,34 @@
 import MySlider from './Slider'
 
 export const Hero = () => {
+	const slides = [
+		{
+			id: 1,
+			url: '../../../public/images/1.jpg',
+			title: 'Збір на 57 бригаду, котрі беруть участь в найзапекліших боях',
+		},
+		{
+			id: 2,
+			url: '../../../public/images/5.jpg',
+			title: 'Збір на 57 бригаду, котрі беруть участь в найзапекліших боях',
+		},
+		{
+			id: 3,
+			url: '../../../public/images/7.jpg',
+			title: 'Збір на 57 бригаду, котрі беруть участь в найзапекліших боях',
+		},
+		{
+			id: 4,
+			url: '../../../public/images/8.jpg',
+			title: 'Збір на 57 бригаду, котрі беруть участь в найзапекліших боях',
+		},
+	]
+
 	return (
 		<div className='hero'>
-			<div className='container container_hero'>
-				<MySlider />
-			</div>
+			{/* <div className='container container_hero'> */}
+			<MySlider slides={slides} />
+			{/* </div> */}
 		</div>
 	)
 }
--- src/components/sections/Slider.jsx
+++ src/components/sections/Slider.jsx
@@ -1,37 +1,80 @@
 import Slider from 'react-slick'
 import 'slick-carousel/slick/slick-theme.css'
 import 'slick-carousel/slick/slick.css'
-export default function MySlider() {
+export default function MySlider({ slides }) {
 	const settings = {
 		dots: true,
 		infinite: true,
 		speed: 500,
 		slidesToShow: 1,
 		slidesToScroll: 1,
+		nextArrow: <SampleNextArrow />,
+		prevArrow: <SamplePrevArrow />,
+
+		customPaging: (dots) => <div></div>,
 	}
 	return (
 		<div>
-			<h2> Single Item</h2>
 			<Slider {...settings}>
-				<div>
-					<h3>1</h3>
-				</div>
-				<div>
-					<h3>2</h3>
-				</div>
-				<div>
-					<h3>3</h3>
-				</div>
-				<div>
-					<h3>4</h3>
-				</div>
-				<div>
-					<h3>5</h3>
-				</div>
-				<div>
-					<h3>6</h3>
-				</div>
+				{slides.map((item) => (
+					<>
+						<div
+							style={{
+								margin: '0 auto',
+								backgroundImage: `url(${item.url})`,
+								backgroundSize: 'cover',
+								width: '100%',
+								height: '730px',
+							}}
+						></div>
+						<div>{item.title}</div>
+					</>
+				))}
 			</Slider>
+		</div>
+	)
+}
+function SampleNextArrow(props) {
+	const { className, style, onClick } = props
+	return (
+		<div
+			style={{
+				display: 'flex',
+				justifyContent: 'end',
+			}}
+			className='container'
+		>
+			<div
+				style={{
+					position: 'absolute',
+					zIndex: '130',
+					top: '50%',
+					display: 'block',
+					background: 'red',
+				}}
+				onClick={onClick}
+			>
+				000
+			</div>
+		</div>
+	)
+}
+function SamplePrevArrow(props) {
+	const { className, style, onClick } = props
+	return (
+		<div className='container'>
+			<div
+				style={{
+					position: 'absolute',
+					zIndex: '100',
+					top: '50%',
+					color: 'black',
+					background: 'red',
+				}}
+				onClick={onClick}
+			>
+				gggg
+			</div>
 		</div>
 	)
 }
