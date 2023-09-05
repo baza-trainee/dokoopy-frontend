87b3c05562e66a0a1cb80c653bc9663c595a8f7b
Andrii <andriy30534>
2023-09-05 19:54:52 +0300

    changed the path icon

--- src/components/MySlider.jsx
+++ src/components/MySlider.jsx
@@ -1,8 +1,8 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
-import arrLeft from "../../assets/chevron-left.svg";
-import arrRight from "../../assets/chevron-right.svg";
+import arrLeft from "../assets/chevron-left.svg";
+import arrRight from "../assets/chevron-right.svg";
 
 export const MySlider = ({ slides }) => {
    const settings = {
