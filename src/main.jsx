d88dab58643f103f2eb14d63834a204f1518fe6a
AscariA10 <109697703+AscariA10@users.noreply.github.com>
2023-09-05 20:15:44 +0300

    add inner routing, add pages 404, all projects, home

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
 import App from "./App.jsx";
 import "./styles/main.scss";
 // import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
    <React.StrictMode>
-      <App />
+      <BrowserRouter>
+         <App />
+      </BrowserRouter>
    </React.StrictMode>
 );
