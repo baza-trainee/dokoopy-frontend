02639e257d8282cde603dbb3084fd18a21c0e250
Andrii <andriy30534>
2023-09-05 18:33:14 +0300

    changed the path fotos

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,32 +1,32 @@
-import MySlider from './Slider'
+import { MySlider } from "./Slider";
 
 export const Hero = () => {
-	const slides = [
-		{
-			id: 1,
-			url: '../../../public/images/1.jpg',
-			title: 'Збір на 57 бригаду, котрі беруть участь в найзапекліших боях',
-		},
-		{
-			id: 2,
-			url: '../../../public/images/5.jpg',
-			title: 'Збір на 57 бригаду, котрі беруть участь в найзапекліших боях',
-		},
-		{
-			id: 3,
-			url: '../../../public/images/7.jpg',
-			title: 'Збір на 57 бригаду, котрі беруть участь в найзапекліших боях',
-		},
-		{
-			id: 4,
-			url: '../../../public/images/8.jpg',
-			title: 'Збір на 57 бригаду, котрі беруть участь в найзапекліших боях',
-		},
-	]
+   const slides = [
+      {
+         id: 1,
+         url: "../../../images/hero_example.jpg",
+         title: "Збір на 57 бригаду, котрі беруть участь в найзапекліших боях",
+      },
+      {
+         id: 2,
+         url: "../../../images/1.jpg",
+         title: "Збір на 57 бригаду, котрі беруть участь в найзапекліших боях",
+      },
+      {
+         id: 3,
+         url: "../../../images/7.jpg",
+         title: "Збір на 57 бригаду, котрі беруть участь в найзапекліших боях",
+      },
+      {
+         id: 4,
+         url: "../../../images/8.jpg",
+         title: "Збір на 57 бригаду, котрі беруть участь в найзапекліших боях",
+      },
+   ];
 
-	return (
-		<div className='hero'>
-			<MySlider slides={slides} />
-		</div>
-	)
-}
+   return (
+      <section className="hero">
+         <MySlider slides={slides} />
+      </section>
+   );
+};
