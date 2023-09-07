ce1bc6ef7820c78a32fc4fb4761029dddc705ddf
Elena_Remennaya <elena.oleksenko12@gmail.com>
2023-09-07 09:33:57 +0300

    add not_found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,3 +1,18 @@
+import { Link } from "react-router-dom";
+
 export const NotFound = () => {
-   return <div>404</div>;
-};
+      return (
+         <div className="wrapper-error">
+            <div className="container">
+          <div className="error-container">
+              <h1 className="error-title">404</h1>
+              <h3 className="error-text-warning">вибачте, такої сторінки не існує</h3>
+              <p className="error-text">Сторінка може бути видалена або тимчасово недоступна</p>
+              <Link to={"/"} className="error-btn">на головну</Link>
+          </div>
+          </div>
+         </div>
+      );
+  
+  }
+
