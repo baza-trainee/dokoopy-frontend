7a0d482f9344a99a42b3a2a82257e5b4efc7913b
AscariA10 <109697703+AscariA10@users.noreply.github.com>
2023-09-08 01:35:21 +0300

    add portal, add some styles to header
    add portal to index.html

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import { DonateButton } from "./DonateButton";
+
 import { Chevron } from "../assets/chevron-down.jsx";
+import { BurgerMenu } from "../assets/burger-menu.jsx";
 
 export const Header = () => {
    return (
@@ -23,6 +25,25 @@ export const Header = () => {
                <p className="language-selector">
                   UA <Chevron />
                </p>
+               <div className="burger-menu">
+                  <BurgerMenu />
+               </div>
+            </div>
+         </div>
+         <div className="mobile-menu">
+            <div className="container">
+               <Link to="/" className="logo logo_mobile-menu">
+                  dokoopy
+               </Link>
+               <nav className="navigation_mobile-menu">
+                  <ul className="navigation-list_mobile-menu">
+                     <li>мета</li>
+                     <li>бригада</li>
+                     <li>
+                        <Link to="allprojects">проєкти</Link>
+                     </li>
+                  </ul>
+               </nav>
             </div>
          </div>
       </header>
