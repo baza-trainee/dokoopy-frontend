d8c28ebb6bdf3712f172e30d2011d5adaa3552c8
AscariA10 <109697703+AscariA10@users.noreply.github.com>
2023-09-13 23:54:18 +0300

    add styles and icons

--- src/components/admin-components/AdminAside.jsx
+++ src/components/admin-components/AdminAside.jsx
@@ -1,25 +1,50 @@
 import { Link } from "react-router-dom";
 
+import { AdminSliderIcon } from "../../assets/admin-icons/admin-slider";
+import { AdminProjectsIcon } from "../../assets/admin-icons/admin-projects";
+import { AdminPartnersIcon } from "../../assets/admin-icons/admin-partners";
+import { AdminContactsIcon } from "../../assets/admin-icons/admin-contacts";
+import { AdminBankIcon } from "../../assets/admin-icons/admin-bank";
+import { AdminReportIcon } from "../../assets/admin-icons/admin-report";
+
 export const AdminAside = () => {
    return (
       <aside className="aside">
          <ul>
             <li className="admin-list-item">
+               <div className="admin-list-icon">
+                  <AdminSliderIcon />
+               </div>
                <Link to="/admin">Hero</Link>
             </li>
             <li className="admin-list-item">
+               <div className="admin-list-icon">
+                  <AdminProjectsIcon />
+               </div>
                <Link to="projects">Проєкти</Link>
             </li>
             <li className="admin-list-item">
+               <div className="admin-list-icon">
+                  <AdminPartnersIcon />
+               </div>
                <Link to="partners">Партнери</Link>
             </li>
             <li className="admin-list-item">
+               <div className="admin-list-icon">
+                  <AdminContactsIcon />
+               </div>
                <Link to="contacts">Контакти</Link>
             </li>
             <li className="admin-list-item">
+               <div className="admin-list-icon">
+                  <AdminBankIcon />
+               </div>
                <Link to="bank-account">Банківські реквізити</Link>
             </li>
             <li className="admin-list-item">
+               <div className="admin-list-icon">
+                  <AdminReportIcon />
+               </div>
                <Link to="reporting">Звітність</Link>
             </li>
          </ul>
