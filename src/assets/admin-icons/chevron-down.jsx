9e14255c115b39958e222667e0005bc978335984
AscariA10 <109697703+AscariA10@users.noreply.github.com>
2023-09-14 18:01:31 +0300

    add some colors and variables

---
+++ src/assets/admin-icons/chevron-down.jsx
@@ -0,0 +1,19 @@
+export const ChevronDown = () => {
+   return (
+      <svg
+         width="20"
+         height="20"
+         viewBox="0 0 20 20"
+         fill="none"
+         xmlns="http://www.w3.org/2000/svg"
+      >
+         <path
+            d="M5 7.5L10 12.5L15 7.5"
+            stroke="#646464"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+         />
+      </svg>
+   );
+};
