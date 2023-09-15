c4578652f7666a6429ef2a8615ecbbaaeb974e63
Andrii <andriy30534>
2023-09-15 12:26:38 +0300

    added contacts from allProject page

--- src/components/SharedLayout.jsx
+++ src/components/SharedLayout.jsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router-dom";
-
-import { Header } from "./Header";
 import { Footer } from "./Footer";
+import { Header } from "./Header";
+import { Contacts } from "./sections/Contacts";
 
 export const SharedLayout = () => {
    return (
       <div>
          <Header></Header>
          <Outlet />
+         <Contacts></Contacts>
          <Footer></Footer>
       </div>
    );
