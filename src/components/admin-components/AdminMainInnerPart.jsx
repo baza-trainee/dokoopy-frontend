19b0178d2f3ab01bbf8870fa40887280594af2bb
Elena_Remennaya <elena.oleksenko12@gmail.com>
2023-09-16 12:26:29 +0300

    fix footer

--- src/components/admin-components/AdminMainInnerPart.jsx
+++ src/components/admin-components/AdminMainInnerPart.jsx
@@ -3,21 +3,21 @@ import { EditIcon } from "../../assets/icon/EditIcon";
 
 export const AdminMainInnerPart = ({name, photo, date, link, data}) => {
     return (
-        <div className="admin-hero-main">
-            <div className="admin-hero-main-header">
-               <div className="admin-hero-main-header-wrapper">
-               <div className="admin-hero-main-header-title">{name}</div>
-               <div className="admin-hero-main-header-title">{photo}</div>
-               <div className="admin-hero-main-header-title">{date ? date : link}</div>
-               <div className="admin-hero-main-header-title"></div>
+        <div className="admin-main-wrapper">
+            <div className="admin-main-header">
+               <div className="admin-main-header-wrapper">
+               <div className="admin-main-header-title">{name}</div>
+               <div className="admin-main-header-title">{photo}</div>
+               <div className="admin-main-header-title">{date ? date : link}</div>
+               <div className="admin-main-header-title"></div>
                </div>
             </div>
-            <ul className="admin-hero-main-content"> 
+            <ul className="admin-main-content"> 
                {data.map(contentData => {
                   return (
-                     <li key={contentData.id} className="admin-hero-main-content-element">
-                        <div className="content-element-wrapper">
-                        <div className="content-element-name">{contentData.name}</div>
+                     <li key={contentData.id} className="admin-main-content-element">
+                        {/* <div className="content-element-wrapper"> */}
+                        <div className="content-element-name"><p>{contentData.name}</p></div>
                         <div className="content-element-img-box">
                            <div className={contentData.date ? "wrapper-img-box" : "wrapper-img-box-svg"}>
                               <img src={contentData.img} alt={contentData.alt}
@@ -29,10 +29,10 @@ export const AdminMainInnerPart = ({name, photo, date, link, data}) => {
                         <div className="content-edit-icon-box">
                            <div className="wrapper-edit-icon-box">
                            <EditIcon />
-                              {/* <div className="content-edit-icon"></div> */}
+                              <div className="content-edit-icon"></div>
                            </div>
                         </div>
-                        </div>
+                        {/* </div> */}
                      </li>
                   );
                })}
