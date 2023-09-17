598a0df6ae080663443fb32e994e5001dd7d2f62
Elena_Remennaya <elena.oleksenko12@gmail.com>
2023-09-17 23:59:12 +0300

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,22 +8,23 @@ export const Footer = () => {
          <div className='footer-logo-container'>
             <div className='logo-svg-container'>
                <LogoFooterIcon />
+               </div>
+               <div className="logo-text">DOkoopy</div>
             </div>
-         <div className='logo-text'>DOkoopy</div>
-         </div>
-         <div className='container-btn'>
-         <DonateButton buttonClass={"footerButton"} />
-         </div>
-         <div className='footer-policy-container'>
-            <div className='footer-policy-link-container'>
-                  <a className='footer-policy-link'>Політика конфіденційності</a>
-                  <a className='footer-policy-link'>
-                     Правила користування сайтом
-                     </a> 
-                  <a className='footer-policy-link'>Звітність</a>
+            <div className="container-btn">
+               <DonateButton buttonClass={"footerButton"} />
             </div>
-            <p className='footer-policy-text'>Розробка Baza Trainee Ukraine 2023 © Всі права захищені</p>
-         </div>
+            <div className="footer-policy-container">
+               <div className="footer-policy-link-container">
+                  <a className="footer-policy-link">Політика конфіденційності</a>
+                  <a className="footer-policy-link">Правила користування сайтом</a>
+                  <a className="footer-policy-link">Звітність</a>
+               </div>
+               <p className="footer-policy-text">
+                  Розробка Baza Trainee Ukraine 2023 © Всі права захищені
+               </p>
+
+            </div>
          </div>
       </footer>
    );
