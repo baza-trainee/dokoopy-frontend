d4b2dec442446b00f4239bd7b64178248c0d3c78
Andrii <andriy30534>
2023-09-17 11:31:15 +0300

    added api

--- src/api/api.js
+++ src/api/api.js
@@ -18,72 +18,81 @@ export const lendingData = {
       return axios.get("contacts");
    },
 };
-export const AdminApi = {
-   config: {
-      headers: {
-         "Content-Type": "multipart/form-data",
-      },
-   },
-   _token: null,
-   // setToken(token) {
-   //    this._token = token;
-   //    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-   // },
-   // loginAdmin() {
-   //    return axios.post("auth/admin/login", {
-   //       headers: {
-   //          "Content-Type": "application/json",
-   //       },
 
-   //       email: "test@test.com",
-   //       password: "test",
-   //    });
-   // },
+export const AdminApi = {
+   loginAdmin(body) {
+      return axios.post("auth/admin/login", body);
+   },
+   logoutAdmin(body) {
+      return axios.get("auth/admin/logout", body);
+   },
+
    //Project
    getProjectAdmin() {
       return axios.get("projects/admin");
    },
    addProject(body) {
-      return axios.post("project/admin", {
-         config,
-         ...body,
-      });
+      return axios.post("project/admin", body);
    },
    updateProject({ id, body }) {
-      return axios.put(`project/admin${id}`, {
-         config,
-         ...body,
-      });
+      return axios.put(`project/admin${id}`, body);
    },
    deleteProject(id) {
       return axios.delete(`projects/admin${id}`);
    },
+
    //Bank
    getBankAdmin() {
       return axios.get("bank/admin");
    },
    updateBank(body) {
-      return axios.get("bank/admin", {
-         ...body,
-      });
+      return axios.get("bank/admin", body);
    },
-   //Hero
 
+   //Heros
    getHerosAdmin() {
       return axios.get("hero/admin");
    },
    addHero(body) {
-      return axios.post("hero/admin", {
-         ...body,
-      });
+      return axios.post("hero/admin", body);
    },
    updateHero({ id, body }) {
-      return axios.put(`hero/admin${id}`, {
-         config,
-         ...body,
-      });
+      return axios.put(`hero/admin${id}`, body);
    },
    deleteHero(id) {
       return axios.delete(`hero/admin${id}`);
+   },
+
+   //Reports
+   addReports(body) {
+      return axios.post("reports/admin", body);
+   },
+   getReportsAdmin() {
+      return axios.get("reports/admin");
+   },
+   updateReports({ id, body }) {
+      return axios.put(`reports/admin${id}`, body);
+   },
+
+   //Contacts
+   updateContacts(body) {
+      return axios.put("contacts/admin", body);
+   },
+   getContactsAdmin() {
+      return axios.get("contacts/admin");
+   },
+
+   //Partners
+   addPartners(body) {
+      return axios.post("partners/admin", body);
+   },
+   getPartnersAdmin() {
+      return axios.get("partners/admin");
+   },
+   updatePartners({ id, body }) {
+      return axios.put(`partners/admin${id}`, body);
+   },
+   deletePartners(id) {
+      return axios.delete(`partners/admin${id}`);
    },
 };
