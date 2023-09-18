c7f4935b9b3dd5293b8c1476939a0648f8e2de1d
Oleksand-Zvirych <oaleks262@gmail.com>
2023-09-18 13:00:24 +0300

    finish style modal adm

--- src/components/admin-components/PageHeader.jsx
+++ src/components/admin-components/PageHeader.jsx
@@ -1,8 +1,23 @@
 import { useNavigate } from "react-router-dom";
 import leftArow from "../../assets/icon/Vector.svg";
 import removeIcon from "../../assets/icon/remove-icon.svg";
+import { AdminModal } from "../admin-components/AdminModal";
+import { useState } from "react";
+
+
 export const PageHeader = ({ title, edit }) => {
    const navigate = useNavigate();
+
+   const [isModalOpen, setIsModalOpen] = useState(false);
+
+   const handleOpenModal = () => {
+      setIsModalOpen(true);
+    };
+  
+    const handleCloseModal = () => {
+      setIsModalOpen(false);
+    };
+
    return (
       <>
          <div className="sub-title-container">
@@ -11,15 +26,16 @@ export const PageHeader = ({ title, edit }) => {
                <h3 className="admin-sub-title">{title}</h3>
             </div>
             {edit && (
-               <button className="remove-btn">
+               <button className="remove-btn" onClick={handleOpenModal}>
                   <div>
-                     <p className="remove-btn-text">Видалити</p>
+                     <p className="remove-btn-text" >Видалити</p>
                      <img src={removeIcon}></img>
                   </div>
                </button>
             )}
          </div>
          <div className="divider"></div>
+         {isModalOpen && <AdminModal onClose={handleCloseModal}/>}
       </>
    );
 };
