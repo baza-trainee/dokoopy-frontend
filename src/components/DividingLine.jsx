634bc663c7b74e29ce12e28e3f6c4a566c3ec6bf
Serhii Onopriienko <seriyrayne@gmail.com>
2023-09-23 23:12:57 +0200

    animatedHashteg

--- src/components/DividingLine.jsx
+++ src/components/DividingLine.jsx
@@ -1,26 +1,50 @@
 import icon from "../assets/icon/Logo_Ukraine_Defense.svg";
 export const DividingLine = ({ mainColor }) => {
-   return (
-      <div className={mainColor + " containerHeshteg "}>
-         <ul className="contentHeshteg ">
-            {mainColor === "whiteContainer" ? (
-               <li></li>
-            ) : (
-               <li>
-                  <img src={icon}></img>
-               </li>
-            )}
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-         </ul>
-      </div>
-   );
+   if (mainColor === "whiteContainer") {
+      return (
+         <div className={mainColor + " containerHeshteg "}>
+            <div className="contentHeshteg">#ХОЛОД У ЖИЛАХ</div>
+            <div className="contentHeshteg">#ЗАПАЛ У СЕРЦЯХ</div>
+            <div className="contentHeshteg">#ХОЛОД У ЖИЛАХ</div>
+            <div className="contentHeshteg">#ЗАПАЛ У СЕРЦЯХ</div>
+            <div className="contentHeshteg">#ХОЛОД У ЖИЛАХ</div>
+            <div className="contentHeshteg">#ЗАПАЛ У СЕРЦЯХ</div>
+            <div className="contentHeshteg">#ХОЛОД У ЖИЛАХ</div>
+            <div className="contentHeshteg">#ЗАПАЛ У СЕРЦЯХ</div>
+         </div>
+      );
+   } else {
+      return (
+         <div className={mainColor + " containerHeshteg "}>
+            <div className="contentHeshteg">
+               <img src={icon} alt="icon" />
+            </div>
+            <div className="contentHeshteg">#МАЙБУТНЄ</div>
+            <div className="contentHeshteg">#СВОБОДА</div>
+            <div className="contentHeshteg">#ЗЕМЛЯ</div>
+            <div className="contentHeshteg">#РОДИНА</div>
+            <div className="contentHeshteg">#СИЛА</div>
+            <div className="contentHeshteg">#ЗАХИСТ</div>
+            <div className="contentHeshteg">#ВОЛЯ</div>
+            <div className="contentHeshteg">#НАЦІЯ</div>
+            <div className="contentHeshteg">
+               <img src={icon} alt="icon" />
+            </div>
+            <div className="contentHeshteg">#МАЙБУТНЄ</div>
+            <div className="contentHeshteg">#СВОБОДА</div>
+            <div className="contentHeshteg">#ЗЕМЛЯ</div>
+            <div className="contentHeshteg">#РОДИНА</div>
+            <div className="contentHeshteg">#СИЛА</div>
+            <div className="contentHeshteg">#ЗАХИСТ</div>
+            <div className="contentHeshteg">#ВОЛЯ</div>
+            <div className="contentHeshteg">#НАЦІЯ</div>
+            <div className="contentHeshteg">
+               <img src={icon} alt="icon" />
+            </div>
+            <div className="contentHeshteg">#МАЙБУТНЄ</div>
+         </div>
+      );
+   }
 };
 
 // для використання цього компоненту необхідно при його вставці додати props -
