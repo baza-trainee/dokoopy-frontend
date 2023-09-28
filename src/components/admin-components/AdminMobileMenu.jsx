8a4275df5ed16f2c4e0c171b43bc734fbf8fb734
AscariA10 <109697703+AscariA10@users.noreply.github.com>
2023-09-28 18:27:49 +0300

    add some styles

---
+++ src/components/admin-components/AdminMobileMenu.jsx
@@ -0,0 +1,20 @@
+import { CloseModal } from "../../assets/icon/close-modal";
+
+export const AdminMobileMenu = () => {
+   return (
+      <div className="admin-mobile-menu-wrapper">
+         <div className="admin-mobile-close-icon">
+            <CloseModal />
+         </div>
+         <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+         </ul>
+      </div>
+   );
+};
