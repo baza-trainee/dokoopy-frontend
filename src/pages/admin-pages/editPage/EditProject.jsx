227c899ef2ff20687e70d91b93c8482ac94347a4
Andrii <andriy30534>
2023-10-03 12:30:16 +0300

    added change

--- src/pages/admin-pages/editPage/EditProject.jsx
+++ src/pages/admin-pages/editPage/EditProject.jsx
@@ -1,4 +1,4 @@
-import { AdminApi } from "../../../api/api.js";
+import { lendingData } from "../../../api/api.js";
 import foto from "../../../assets/images/default-image.jpg";
 import { AddForm } from "../../../components/admin-components/AddForm";
 import { PageHeader } from "../../../components/admin-components/PageHeader";
@@ -13,17 +13,15 @@ export const EditProject = () => {
       descriptionENG:
          "We are the 57th brigade and participate in the fiercest battles. Therefore, the need for consumables is simply veeleetenskaya - these are walkie-talkies, car repairs, tires on wheels, etc.",
    };
-   const { data } = useLoadingData(AdminApi.getHerosAdmin);
+   const { data } = useLoadingData(lendingData.getProject);
    console.log(data);
-   const submitClick = data => {
-      console.log(data);
-   };
+   const submitClick = data => {};
    return (
       <section className="page-container">
          <PageHeader edit={true} title={"Редагувати проєкт"} />
          <AddForm
             // smPlaceholder={"Введіть назву проєкту"}
-            // lgPlaceholder={"Додайте опис проєкту"}
+            // lgPlaceholder={"Додайте опис проєкту"}`
             lgLiable={"Опис проєкту*"}
             smLiable={"Назва проєкту*"}
             nameButton={"Внести зміни"}
