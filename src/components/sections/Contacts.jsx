21d6b34d33beee93b99a6c895eddbd360756a1b1
Oleksandr <admin@admins-MacBook-Pro.local>
2023-10-03 09:03:36 +0300

    edit

--- src/components/sections/Contacts.jsx
+++ src/components/sections/Contacts.jsx
@@ -1,46 +1,26 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-import { lendingData } from "../../api/api";
-
 export const Contacts = () => {
-  const [contactData, setContactData] = useState({
-    email: "eugene@dontpanic.team",
-    telegram: "https://t.me/Boooble_boo",
-  });
-
-  useEffect(() => {
-    lendingData.getContact()
-      .then((response) => {
-        setContactData(response.data || {
-          email: "eugene@dontpanic.team",
-          telegram: "https://t.me/Boooble_boo",
-        });
-      })
-      .catch((error) => {
-        console.error("Помилка отримання контактів:", error);
-      });
-  }, []);
-
-  return (
-    <div className="contact">
-      <div className="container">
-        <div className="contact-box">
-          <div className="contact-box-title">
-            <h2 className="contact-box-title-h2">Звернутись до організаторів</h2>
-            <p className="contact-box-title-p">
-              Якщо готові скластись dokoopy і долучитись до хорошої справи – пишіть прямо зараз ось сюди:
-            </p>
-          </div>
-          <div className="contact-link">
-            <a className="navigation-list-item" href={`mailto:${contactData.email}`}>
-              email
-            </a>
-            <a className="navigation-list-item" href={contactData.telegram} target="_blank">
-              telegram
-            </a>
-          </div>
-        </div>
+   return (
+      <div className="contact">
+         <div className="container">
+            <div className="contact-box">
+            <div className="contact-box-title">
+               <h2 className="contact-box-title-h2">Звернутись до організаторів</h2>
+               <p className="contact-box-title-p">
+                  Якщо готові скластись dokoopy і долучитись до хорошої справи – пишіть прямо зараз
+                  ось сюди:
+               </p>
+               </div>
+               <div className="contact-link ">
+                  <a className="navigation-list-item" href="mailto:eugene@dontpanic.team">
+                     email
+                     </a>
+                  <a className="navigation-list-item" href="https://t.me/Boooble_boo" target="_blanck">
+                     telegram
+                  </a>
+               </div>
+               
+            </div>
+         </div>
       </div>
-    </div>
-  );
+   );
 };
