e4bf69d6ae3381e10d10182a3e385a6920157c57
Andrii <andriy30534>
2023-10-04 17:25:56 +0300

    add data

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,9 +1,5 @@
 import { useEffect, useState } from "react";
 import { lendingData } from "../../api/api";
-import foto3 from "../../assets/images/8.jpg";
-import foto4 from "../../assets/images/about_img.jpg";
-import foto1 from "../../assets/images/hero_example.jpg";
-import foto5 from "../../assets/images/photo5.jpeg";
 import { useLoadingData } from "../../hook/useLoadingData";
 import { MySlider } from "../landing-components/MySlider";
 import { useLandingContext } from "../provider-components/landing-provider";
@@ -23,33 +19,10 @@ export const Hero = () => {
          );
       }
    }, [data?.heroes, language]);
-   console.log(slides);
-   const slidess = [
-      {
-         id: 1,
-         imageURL: foto1,
-         description: "Збір для воїнів 57 бригади, які беруть участь в найзапекліших боях!",
-      },
 
-      {
-         id: 3,
-         imageURL: foto3,
-         description: "Наш внесок - це шлях до перемоги наших військових!",
-      },
-      {
-         id: 4,
-         imageURL: foto4,
-         description: "Захистимо разом наших Героїв! ",
-      },
-      {
-         id: 5,
-         imageURL: foto5,
-         description: "Об'єднаймо зусилля для підтримки 57-ї бригади!",
-      },
-   ];
    return (
       <section className="hero">
-         <MySlider slides={slidess} />
+         <MySlider slides={slides} />
       </section>
    );
 };
