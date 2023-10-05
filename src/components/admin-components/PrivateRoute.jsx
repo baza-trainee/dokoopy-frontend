0fed5f5f30be5547697607a8b88b78256e377924
AscariA10 <109697703+AscariA10@users.noreply.github.com>
2023-10-06 00:22:37 +0300

    add private routes, and login forms

--- src/components/admin-components/PrivateRoute.jsx
+++ src/components/admin-components/PrivateRoute.jsx
@@ -1,7 +1,7 @@
 import { useAppContext } from "../provider-components/app-provider";
 import { Navigate } from "react-router-dom";
 
-export const PrivateRoute = ({ component: Component, redirectTo = "/admin" }) => {
+export const PrivateRoute = ({ component: Component, redirectTo = "/" }) => {
    const { loggedIn } = useAppContext();
    const shouldRedirect = !loggedIn;
 
