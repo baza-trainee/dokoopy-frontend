f62ec9fa33b0e1ff3c7788a9d49af5767287229b
Andrii <andriy30534>
2023-10-10 18:59:22 +0300

    change loader

--- src/components/admin-components/AdminModal.jsx
+++ src/components/admin-components/AdminModal.jsx
@@ -23,36 +23,30 @@ export const AdminModal = ({ onClose, removeItem, success, elementType }) => {
    };
 
    return (
-      <>
-         <div className="admin-modal">
-            {isLoading ? (
-               <Spinner size={300} color={"#2672e4"} />
-            ) : (
-               <div className="admin-modal-window">
-                  <div className="admin-modal-button-x">
-                     <button className="admin-button-close" onClick={handleCloseClick}>
-                        <img src={iconx} className="iconx-img" alt="Закрити" />
-                     </button>
-                  </div>
-                  <div className="admin-modal-text">
-                     <h2>
-                        Ви впевнені, що хочете
-                        <br />
-                        видалити {elementType}?
-                     </h2>
+      <div className="admin-modal">
+         <div className="admin-modal-window">
+            <div className="admin-modal-button-x">
+               <button className="admin-button-close" onClick={handleCloseClick}>
+                  <img src={iconx} className="iconx-img" alt="Закрити" />
+               </button>
+            </div>
+            <div className="admin-modal-text">
+               <h2>
+                  Ви впевнені, що хочете
+                  <br />
+                  видалити {elementType}?
+               </h2>
 
-                     <div className="admin-modal-button">
-                        <button className="admin-modal-no" onClick={handleCloseClick}>
-                           <h4>Ні</h4>
-                        </button>
-                        <button onClick={handlerRemove} className="admin-modal-yes">
-                           <h4>Так</h4>
-                        </button>
-                     </div>
-                  </div>
+               <div className="admin-modal-button">
+                  <button className="admin-modal-no" onClick={handleCloseClick}>
+                     <h4>Ні</h4>
+                  </button>
+                  <button onClick={handlerRemove} className="admin-modal-yes">
+                     {!isLoading ? <h4>Так</h4> : <Spinner size={30} color={"#ffffff"} />}
+                  </button>
                </div>
-            )}
+            </div>
          </div>
-      </>
+      </div>
    );
 };
