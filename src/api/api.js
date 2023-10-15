2ff62412182f6f873b1aac917b0f1161dfc1426d
Elena_Remennaya <elena.oleksenko12@gmail.com>
2023-10-15 19:15:25 +0300

    add changes

--- src/api/api.js
+++ src/api/api.js
@@ -67,11 +67,11 @@ export const AdminApi = {
    },
 
    //Bank
-   getBankAdmin() {
-      return axios.get(`bank/admin`);
+   getBankAdmin(id) {
+      return axios.get(`bank/admin${id}`);
    },
-   updateBank({ id, body }) {
-      return axios.patch(`bank/admin/${id}`, body);
+   updateBank({ body }) {
+      return axios.patch(`bank/admin${id}`, body);
    },
 
    //Heros
