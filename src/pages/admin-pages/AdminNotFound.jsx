72988767bce9dee8431a72080226114fe7e9f183
Olena Kasianenko <kasianenko.elena.a@gmail.com>
2023-10-18 22:35:20 +0200

    404 fixed

--- src/pages/admin-pages/AdminNotFound.jsx
+++ src/pages/admin-pages/AdminNotFound.jsx
@@ -2,10 +2,12 @@ import { Link } from "react-router-dom";
 
 export const AdminNotFound = () => {
    return (
-      <div className="admin-wrapper-error">       
+      <div className="admin-wrapper-error">   
+         <div className="admin-error-content">
             <h1 className="admin-error-title">404</h1>
             <h3 className="admin-error-text-warning">На жаль, сторінку не знайдено</h3>
             <p className="admin-error-text">Сторінка, яку ви  намагаєтесь знайти не існує, або була видалена</p>
+         </div>  
             <Link to={"/admin"} className="admin-error-btn">
                Ви можете перейти на головну сторінку
             </Link>          
