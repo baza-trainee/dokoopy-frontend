52be36e3ca225e84bb16b7d61900ef7483043934
Olena Kasianenko <kasianenko.elena.a@gmail.com>
2023-10-20 23:03:02 +0200

    AdminModalSuccessful.jsx: Closing a modal window

--- src/components/admin-components/AdminModalSuccessful.jsx
+++ src/components/admin-components/AdminModalSuccessful.jsx
@@ -1,36 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import { CloseModal } from "../../assets/icon/close-modal";
 import checkMark from "../../assets/icon/check-mark.svg";
-import { useNavigate } from "react-router-dom";
 
-export const AdminModalSuccessful = () => {
-   const navigate = useNavigate();
-   const [isModalOpen, setIsModalOpen] = useState(false);
-
-   const openModal = () => {
-      setIsModalOpen(true);
-   };
-
-   const closeModal = () => {
-      setIsModalOpen(false);
-   };
-
-   return (
-     <div className={`backdrop ${isModalOpen ? "" : "is-hidden"} data-modal`}>
-         <div className="modal-successful">
-            <div className="modal-content">
-         
-               <div className="modal-close-btn">
-                  <CloseModal onClick={closeModal}>Закрити</CloseModal>                
-               </div>
-               <div className="check-mark-modal">
-                  <img src={checkMark} alt="Check Mark" />
-               </div>
-               <b className="modal-content-text">Пароль успішно змінено!</b>
-               <button className="admin-button" onClick={() => navigate("/admin")}>Закрити</button>          
-            </div>         
-       </div>
+export const AdminModalSuccessful = ({ isModalTrue, closeModal }) => {
+  const handleModalClose = () => {
+    closeModal();
+  };
+  return (
+    <div className={`backdrop ${isModalTrue ? "" : "is-hidden"}`}>
+      <div className="modal-successful">
+        <div className="modal-content">          
+            <button onClick={handleModalClose} className="modal-close-btn">
+              <CloseModal />
+            </button>          
+          <div className="check-mark-modal">
+            <img src={checkMark} alt="Check Mark" />
+          </div>
+          <b className="modal-content-text">Пароль успішно змінено!</b>
+          <button className="admin-button" onClick={handleModalClose}>
+            Закрити
+          </button>
+        </div>
       </div>
-   );
+    </div>
+  );
 };
-
