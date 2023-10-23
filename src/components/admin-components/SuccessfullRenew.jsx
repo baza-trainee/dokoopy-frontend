a46a052011a6e8c714d848abb8f8cbc7e1f1f805
Serhii Onopriienko <seriyrayne@gmail.com>
2023-10-23 23:58:00 +0200

    moreFixes

--- src/components/admin-components/SuccessfullRenew.jsx
+++ src/components/admin-components/SuccessfullRenew.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 const SuccessfullRenew = () => {
    const navigate = useNavigate();
    return (
-      <div className="login-content renew-content">
+      <div className="login-content">
          <img className="check-mark" src={checkMark} />
          <h2 className="successfullH2">Пароль успішно змінено!</h2>
          <form className="login-form success-form">
